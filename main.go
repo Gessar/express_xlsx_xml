a7dce6b2732342b14b0222e9b39e5e2da70990c7
Gessar <Gessar2010@gmail.com>
2024-05-06 11:12:29 +0500

    asd

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/xuri/excelize/v2"
+)
 
 func main() {
-	fmt.Println("test1")
+	// Открываем файл XLSX для редактирования
+	f, err := excelize.OpenFile("samplefile.xlsx")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Создаем новый лист с именем "Новый лист"
+	index, err := f.NewSheet("Новый лист")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Устанавливаем активным листом только что созданный
+	f.SetActiveSheet(index)
+
+	// Сохраняем изменения обратно в файл XLSX
+	if err := f.Save(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Лист успешно добавлен в файл samplefile.xlsx!")
 }
